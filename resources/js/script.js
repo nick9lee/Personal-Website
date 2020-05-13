923e9cfdbd957eb63ba65cb54c23a1d2c4f6e583
Nick Lee <Nicholas9lee@gmail.com>
2020-05-13 14:07:30 -0600

    finished responsive menu

--- resources/js/script.js
+++ resources/js/script.js
@@ -24,6 +24,21 @@ $(document).ready(function() {
         $('html, body').animate({scrollTop: $('.js--contacts').offset().top}, 1000);
     });
     
-    /*---animations on scroll---*/
+    /*------mobile nav-----------*/
     
+    
+    /*---animations on scroll---*/
+    $('.js--nav-icon').click(function(){
+        var nav = $('.js--main-nav');
+        var icon = $('.js--nav-icon i');
+        nav.slideToggle(200);
+        
+        if(icon.hasClass('ion-navicon-round')){
+            icon.addClass('ion-close-round');
+            icon.removeClass('ion-navicon-round');
+        } else{
+            icon.addClass('ion-navicon-round');
+            icon.removeClass('ion-close-round');
+        }
+    });
 });
